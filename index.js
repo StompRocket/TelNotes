2722600de7e748b025decd6224082a4774171c02
Rosho123 <rsf.sho@gmail.com>
2017-05-06 19:48:49 -0700

    deleted unnecessary code

--- index.js
+++ index.js
@@ -1,49 +1,20 @@
 var net = require('net')
-var count = 0,
-	users = {};
 var server = net.createServer(function (conn) {
 	console.log('\033[90m new connection!\033[39m');
 	conn.write(
-		'\n > welcome to \033[92mnode-chat\033[39m!' +
-		'\n > ' + count + ' other people on this server' +
-		'\nplease write your name and hit enter: '
+		
 	);
 	count++;
 	conn.setEncoding('utf8');
-	var nickname;
+	
 	conn.on('data', function (data) {
 		console.log(data);
-		data = data.replace('\r\n', '');
-		if (!nickname) {
-			if (users[data]) {
-				conn.write('\033[93m> nickname already in use. try again: \033[39m');
-				return;
-			} else {
-				nickname = data;
-				users[nickname] = conn;
-
-				broadcast('\033[90m >' + nickname + ' joined\033[39m\n');
-
-			}
-		} else {
-
-
-			broadcast('\033[96m >' + nickname + ': \033[39m' + data + '\n');
-
-		}
+		
 	});
 
-	function broadcast(msg, execptMe) {
-		for (var i in users) {
-			if (!execptMe || i != nickname) {
-				users[i].write(msg);
-			}
-		}
-	}
+
 	conn.on('close', function () {
-		count--;
-		delete users[nickname];
-		broadcast('\033[90m > ' + nickname + ' Left The Room\033[39\n');
+		
 	});
 });
 
