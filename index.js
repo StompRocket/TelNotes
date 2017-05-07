317c8d044d51451b44cb112b9cee63501ed61109
Rosho123 <rsf.sho@gmail.com>
2017-05-07 13:04:15 -0700

    Working more on sign up flow

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var net = require('net')
+var users = {};
 var server = net.createServer(function (conn) {
 	console.log('\033[90m new connection!\033[39m');
 	conn.setEncoding('utf8');
@@ -16,6 +17,16 @@ var server = net.createServer(function (conn) {
 					'please create a username: '
 				);
 				signup = true;
+			}
+			if (signup) {
+				username = data;
+				if (users[data]){
+					conn.write('\033[93m> Username already in use. try again: \033[39m');
+				return;
+				} else {
+					users[username] = conn;
+				}
+				
 			}
 		}
 
